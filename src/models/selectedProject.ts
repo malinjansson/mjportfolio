8002bb100257b4ecff55cc94ad4c23fc840142b7
Malin Jansson <maalin.jaansson@gmail.com>
2025-05-06 21:48:53 +0200

    created a type for selected projects and added top five project I want to show on the homepage

---
+++ src/models/selectedProject.ts
@@ -0,0 +1,29 @@
+import { Project } from "./types/Project";
+
+export const selectedProject: Project[] = [
+    {
+        id: 1,
+        title: "Auction House",
+        productCard: "/src/assets/ProductCardAuctionHouse.jpg",
+    },
+    {
+        id: 2,
+        title: "Liora",
+        productCard:"/src/assets/ProductCardLiora.jpg",
+    },
+    {
+        id: 3,
+        title: "MovieCloud",
+        productCard:"/src/assets/ProductCardMovieCloud.jpg",
+    },
+    {
+        id: 4,
+        title: "Poster Stories",
+        productCard:"/src/assets/ProductCardPosterStories.jpg",
+    },
+    {
+        id: 5,
+        title: "Todo list",
+        productCard:"/src/assets/ProductCardTodo.jpg",
+    },
+]
