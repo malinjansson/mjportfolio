d57351c2db7a7606c78e8a8f745155ce6f2a8d91
Malin Jansson <maalin.jaansson@gmail.com>
2025-05-08 12:38:10 +0200

    added a skills scroll banner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -37,6 +37,29 @@ export const Home = () => {
                     <li>Canva</li>
                 </ul>
         </div>
+
+        <div className="skills-scroll-banner">
+            <div className="skills-scroll-content">
+                <span>
+                    ~ Frontend ~ 
+                    Backend ~ 
+                    System development ~ 
+                    E-commerce systems ~ 
+                    Payment systems ~ 
+                    Database-driven applications 
+                    ~ API development  
+                </span>
+                <span>
+                    ~ Frontend ~ 
+                    Backend ~ 
+                    System development ~ 
+                    E-commerce systems ~ 
+                    Payment systems ~ 
+                    Database-driven applications 
+                    ~ API development ~  
+                </span>
+            </div>
+        </div>
         </>
     )
 }
