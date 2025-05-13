76ea2230678320987771636704f70613b1806867
Malin Jansson <maalin.jaansson@gmail.com>
2025-05-13 23:44:15 +0200

    added a wave on the intro

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ import "./../styles/home.scss";
 export const Home = () => {
     return (
         <>
+        <div className="intro-wrapper">
         <div className="intro-container">
             <div className="intro-box">
                 <span>
@@ -16,7 +17,15 @@ export const Home = () => {
                 <img className="profile-img" src="src/assets/profileImg.jpg" alt="Profile image"/>
             </div>
         </div>
-
+        <svg className="wave-divider" viewBox="0 0 1440 320" preserveAspectRatio="none">
+                <path d="M0,160 
+                    C540,-80 1080,400 1620,160 
+                    L1620,320 L0,320 Z"
+                    fill="#eaebe8"
+                />
+        </svg>
+        </div>
+    
         <div className="projects-container">
             <h3>Selected projects</h3>
             <ShowSelectedProjects projects={selectedProject} />
