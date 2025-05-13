59fec5f8cc6f87f092a33b4cea660106cc8b077f
Malin Jansson <maalin.jaansson@gmail.com>
2025-05-13 16:39:52 +0200

    added so that the user always comes to the top when navigating to the project detail page

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,12 +1,18 @@
-import { useParams } from "react-router"
+import { useLocation, useParams } from "react-router"
 import { ProjectDetails } from "../models/types/ProjectDetails";
 import { ShowProjectDetails } from "../components/ShowProjectDetails";
 import { getProjectBySlug } from "../utils/projectUtils";
 import "./../styles/projectdetails.scss";
+import { useEffect } from "react";
 
 export const Project = () => {
     const {slug} = useParams();
-
+    const location = useLocation();
+      
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
+      
     const project: ProjectDetails  | undefined = slug
     ? getProjectBySlug(slug) : undefined;
 
