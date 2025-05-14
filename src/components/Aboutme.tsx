e2fca2a75cc01bdd63c4c826204c0a194cd17c67
Malin Jansson <maalin.jaansson@gmail.com>
2025-05-14 16:21:01 +0200

    updated the text

--- src/components/Aboutme.tsx
+++ src/components/Aboutme.tsx
@@ -12,7 +12,7 @@ export const Aboutme = () => {
                 <div className="aboutme-info">
                     <span>I have a Bachelor's Degree in Media and Communication Science and am currently expanding my skills in web development, focusing on e-commerce. I will graduate from Medieinstitutet in May 2026.</span>
                     <span>With previous experience in digital marketing, CMS and content migration, I have closely collaborated with development, design and e-commerce teams. I'm a positive problem solver who thrives in collaborative and creative environments. I possess a high level of commitment and a strong willingness to further expand my skills in both frontend and backend technologies.</span>
-                    <span>I'm looking for an internship between week 40-49. I'm excited to learn as much as possible and contribute my skills to the team. My goal is to gain an opportunity for future employment.</span>
+                    <span>I'm looking for an internship between September 29 and December 5 {"(week 40-49)"}. I'm excited to learn as much as possible and contribute my skills to the team, with the ambition to get a future employment.</span>
                 </div>
                 </div>
             </div>
