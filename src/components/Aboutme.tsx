278a31e5c51fa6989774dec32e3644cbc19b8ce7
Malin Jansson <maalin.jaansson@gmail.com>
2025-05-14 14:02:16 +0200

    updated html structure on aboutme

--- src/components/Aboutme.tsx
+++ src/components/Aboutme.tsx
@@ -4,17 +4,17 @@ export const Aboutme = () => {
     return (
         <>
         <div className="aboutme-container">
-            <div className="aboutme-top-heading">
+            <div className="aboutme-box">
                 <span>About me</span>
-                <div>
+                <div className="internship-wrapper">
                     <h3>Looking for an internship</h3>
                     <h3>Fall 2025</h3>
+                <div className="aboutme-info">
+                    <span>I have a Bachelor's Degree in Media and Communication Science and am currently expanding my skills in web development, focusing on e-commerce. I will graduate from Medieinstitutet in May 2026.</span>
+                    <span>With previous experience in digital marketing, CMS and content migration, I have closely collaborated with development, design and e-commerce teams. I'm a positive problem solver who thrives in collaborative and creative environments. I possess a high level of commitment and a strong willingness to further expand my skills in both frontend and backend technologies.</span>
+                    <span>I'm looking for an internship between week 40-49. I'm excited to learn as much as possible and contribute my skills to the team. My goal is to gain an opportunity for future employment.</span>
                 </div>
-            </div>
-            <div className="aboutme-info">
-                <p>I have a Bachelor's Degree in Media and Communication Science and am currently expanding my skills in web development, focusing on e-commerce. I will graduate from Medieinstitutet in May 2026.</p>
-                <p>With previous experience in digital marketing, CMS and content migration, I have closely collaborated with development, design and e-commerce teams. I'm a positive problem solver who thrives in collaborative and creative environments. I possess a high level of commitment and a strong willingness to further expand my skills in both frontend and backend technologies.</p>
-                <p>I'm looking for an internship between week 40-49. I'm excited to learn as much as possible and contribute my skills to the team. My goal is to gain an opportunity for future employment.</p>
+                </div>
             </div>
         </div>
         </>
