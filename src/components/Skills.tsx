bd63566051299cc9a41ad442c119a4e56633e0a3
Malin Jansson <maalin.jaansson@gmail.com>
2025-05-14 15:03:42 +0200

    added skills banner container

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,26 +1,29 @@
 export const Skills = () => {
     return (
-        <div className="skills-scroll-banner">
-        <div className="skills-scroll-content">
-            <span>
-                ~ Frontend ~ 
-                Backend ~ 
-                System development ~ 
-                E-commerce systems ~ 
-                Payment systems ~ 
-                Database-driven applications 
-                ~ API development  
-            </span>
-            <span>
-                ~ Frontend ~ 
-                Backend ~ 
-                System development ~ 
-                E-commerce systems ~ 
-                Payment systems ~ 
-                Database-driven applications 
-                ~ API development ~  
-            </span>
+        <div className="skills-banner-container">
+            <div className="skills-scroll-banner">
+                <div className="skills-scroll-content">
+                    <span>
+                        ~ Frontend ~ 
+                        Backend ~ 
+                        System development ~ 
+                        E-commerce systems ~ 
+                        Payment systems ~ 
+                        Database-driven applications 
+                        ~ API development  
+                    </span>
+                    <span>
+                        ~ Frontend ~ 
+                        Backend ~ 
+                        System development ~ 
+                        E-commerce systems ~ 
+                        Payment systems ~ 
+                        Database-driven applications 
+                        ~ API development ~  
+                    </span>
+                </div>
+            </div>
         </div>
-    </div>
+      
     );
 };
